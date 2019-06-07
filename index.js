66350f90086c6f32d0cb727db481f07793098494
Melina Carniel <melinagarciac@gmail.com>
2019-06-07 14:39:56 -0300

    separando em rotas

--- index.js
+++ index.js
@@ -5,48 +5,83 @@ const app = express();
 const url = 'https://jsonplaceholder.typicode.com/users';
 
 const PORT = process.env.PORT || 8080;
+let arrayDados = [];
+
+request({ url: url, json: true }, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    arrayDados = response.body;
+  }
+});
 
 app.get('/', (req, res) => {
-  request({ url: url, json: true }, (error, response) => {
-    if (error) {
-      console.log(error);
-      res.send('Erro ao pegar dados!');
-    } else {
-      let dadosSuite = [];
-      let dadosNome = [];
-      let websites = [];
-      let arrayDados = response.body;
-      arrayDados.filter(x => {
-        let string = x.address.suite.toUpperCase();
-        let substring = 'suite';
-        let dadosweb = x.website;
-        let nomes = {
-          nome: x.name,
-          email: x.email,
-          empresa: x.company.name
-        };
-        websites.push(dadosweb);
-        dadosNome.push(nomes);
+  res.send(
+    '<html><h3>Teste Mutant</h3><hr /><div><a href="/1">Item 1</a></div><div><a href="/2">Item 2</a></div><div><a href="/3">Item 3</a></div></html>'
+  );
+});
 
-        if (string.indexOf(substring.toUpperCase()) !== -1) {
-          dadosSuite.push(x.username);
-        }
-      });
+app.get('/1', (req, res) => {
+  if (arrayDados.length == 0) {
+    console.log(error);
+    res.send('Não existe dados!');
+  } else {
+    let websites = [];
+    arrayDados.filter(x => {
+      let dadosweb = x.website;
+      websites.push(dadosweb);
+    });
+    let result = {
+      websites: websites
+    };
+    res.json(result);
+  }
+});
 
-      dadosNome.sort((a, b) => {
-        let textA = a.nome.toUpperCase();
-        let textB = b.nome.toUpperCase();
-        return textA < textB ? -1 : textA > textB ? 1 : 0;
-      });
-
-      let result = {
-        websites: websites,
-        dadosPorNome: dadosNome,
-        dadosSuite: dadosSuite
+app.get('/2', (req, res) => {
+  if (arrayDados.length == 0) {
+    console.log(error);
+    res.send('Não existe dados!');
+  } else {
+    let dadosNome = [];
+    arrayDados.filter(x => {
+      let nomes = {
+        nome: x.name,
+        email: x.email,
+        empresa: x.company.name
       };
-      res.json(result);
-    }
-  });
+      dadosNome.push(nomes);
+    });
+    dadosNome.sort((a, b) => {
+      let textA = a.nome.toUpperCase();
+      let textB = b.nome.toUpperCase();
+      return textA < textB ? -1 : textA > textB ? 1 : 0;
+    });
+    let result = {
+      dadosPorNome: dadosNome
+    };
+    res.json(result);
+  }
+});
+
+app.get('/3', (req, res) => {
+  if (arrayDados.length == 0) {
+    console.log(error);
+    res.send('Não existe dados!');
+  } else {
+    let dadosSuite = [];
+    arrayDados.filter(x => {
+      let string = x.address.suite.toUpperCase();
+      let substring = 'suite';
+      if (string.indexOf(substring.toUpperCase()) !== -1) {
+        dadosSuite.push(x.username);
+      }
+    });
+    let result = {
+      dadosSuite: dadosSuite
+    };
+    res.json(result);
+  }
 });
 
 app.listen(PORT, () => {
